c0dc7c71a71df246b815831881e4f02fd8634714
Zhitao (Reid) Zhang <zzt124@cs.washington.edu>
2017-11-29 11:31:50 -0800

    change freedom loading process

--- src/nwjs/main.js
+++ src/nwjs/main.js
@@ -1,24 +1,27 @@
 "use strict";
 // Load freedom module by loading it as exteranl module. In order to get freedom
 // we has to bind a callback function while it is ready (finished loading).
-function loadScript(url, callback){
-    var foo1 = document.createElement('script');
-    foo1.type = 'text/javascript';
-    foo1.setAttribute('src', url);
+// function loadScript(url, callback){
+//     var foo1 = document.createElement('script');
+//     foo1.type = 'text/javascript';
+//     foo1.setAttribute('src', url);
 
-    foo1.onreadystatechange = callback;
-    foo1.onload = callback;
+//     foo1.onreadystatechange = callback;
+//     foo1.onload = callback;
 
-    var head = document.getElementsByTagName('head')[0];
-    head.appendChild(foo1, head);
-};
-loadScript('./freedom-for-chrome/freedom-for-chrome.js', function(){
-    var uProxyAppChannelPromise = freedom('generic_core/freedom-module.json', {
-        'logger': 'lib/loggingprovider/freedom-module.json',
-        'debug': 'debug',
-        'portType': 'worker'
-    }).then(function (uProxyModuleFactory) {
-        console.log('Core loading complete');
-        return uProxyModuleFactory();
-    });
+//     var head = document.getElementsByTagName('head')[0];
+//     head.appendChild(foo1, head);
+// };
+// loadScript('./freedom-for-chrome/freedom-for-chrome.js', function(){
+
+// });
+// in freedom-for-chrome.js, change {source : findSrc} to {source : window.location.origin + '/freedom-for-chrome/freedom-for-chrome.js'}
+require('./freedom-for-chrome/freedom-for-chrome');
+var uProxyAppChannelPromise = freedom('generic_core/freedom-module.json', {
+    'logger': 'lib/loggingprovider/freedom-module.json',
+    'debug': 'debug',
+    'portType': 'worker'
+}).then(function (uProxyModuleFactory) {
+    console.log('Core loading complete');
+    return uProxyModuleFactory();
 });
