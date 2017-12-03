4cdbaf00dcd4d2e20e869271f585a86b5865a4a1
Zhitao (Reid) Zhang <zzt124@cs.washington.edu>
2017-12-02 20:09:28 -0800

    update the desktop_core_connector as the UI and core runs in the same context

--- src/nwjs/scripts/desktop_core_connector.ts
+++ src/nwjs/scripts/desktop_core_connector.ts
@@ -1,6 +1,4 @@
-
 /**
- * TODO: modify according to cca implementation
  * desktop_core_connector.ts
  *
  * Runs in the UI context, proxying on() and emit() calls to the Freedom app in the
@@ -10,124 +8,113 @@
 import * as browser_connector from '../../interfaces/browser_connector';
 import * as uproxy_core_api from '../../interfaces/uproxy_core_api';
 
-import CoreConnector from '../../generic_ui/scripts/core_connector';
-
-declare const freedom: freedom.FreedomInCoreEnv;
-
-interface OnEmitModule extends freedom.OnAndEmit<any,any> {};
-interface OnEmitModuleFactory extends
-  freedom.FreedomModuleFactoryManager<OnEmitModule> {};
-
-console.log('Loading core');
-// TODO: Handle failure.
-var uProxyAppChannelPromise = freedom(
-    'generic_core/freedom-module.json',
-    <freedom.FreedomInCoreEnvOptions>{
-      'logger': 'lib/loggingprovider/freedom-module.json',
-      'debug': 'debug',
-      'portType': 'worker'
-    }
-).then((uProxyModuleFactory:OnEmitModuleFactory) => {
-  console.log('Core loading complete');
-  return uProxyModuleFactory();
-});
-
-export function makeCoreConnector() : Promise<CoreConnector> {
-  return uProxyAppChannelPromise.then((channel) => {
-    // const browserConnector = new CordovaCoreConnector(channel, {
-    //   name: 'uproxy-ui-to-core-connector'
-    // });
-    // const core = new CoreConnector(browserConnector);
-    return core.login({
-      network: 'Cloud',
-      loginType: uproxy_core_api.LoginType.INITIAL,
-    }).then((loginResult) => {
-      console.debug(`Logged in to Cloud network. userId: ${loginResult.userId}, instanceId: ${loginResult.instanceId}`);
-      return core;
-    });
-  });
-}
+import * as user_interface from '../../generic_ui/scripts/ui'
 
 // UI should talk to core via DesktopCoreConnector, which wraps the uProxy.core
 class DesktopCoreConnector implements browser_connector.CoreBrowserConnector {
-  // Status object indicating whether we're connected to the app.
-  public status :browser_connector.StatusObject;
-
-  private fulfillConnect :Function;
-  public onceConnected :Promise<void> = new Promise<void>((F, R) => {
-    this.fulfillConnect = F;
-  });
-
-  constructor(private appChannel: freedom.OnAndEmit<any,any>,
-      private options ?:chrome.runtime.ConnectInfo) {
-    this.status = { connected: false };
-  }
-
-
-  // --- Connectivity methods ---
-
-  /**
-   * Connect the UI to the Freedom module.
-   *
-   * Returns a promise fulfilled upon connection.
-   */
-  public connect = () : Promise<void> => {
-    console.log('DesktopCoreConnector.connect()');
-    if (!this.status.connected) {
-      this.fulfillConnect();
-      this.emit('core_connect');
-      this.status.connected = true;
-    }
-    return Promise.resolve();
-  }
-
-  // --- Communication ---
-  /**
-   * Attach handlers for updates emitted from the uProxy Core.
-   */
-  public onUpdate = (update :uproxy_core_api.Update,
-      handler :(eventData:any) => void) => {
-    this.onceConnected.then(() => {
-      var type = '' + update;
-      this.appChannel.on(type, handler);
+  private appChannel_ :freedom.OnAndEmit<any,any>;
+  
+    // Status object indicating whether we're connected to the app.
+    public status :browser_connector.StatusObject;
+  
+    private fulfillConnect_ :Function;
+    public onceConnected :Promise<void> = new Promise<void>((F, R) => {
+      this.fulfillConnect_ = F;
     });
-  }
-
-  /**
-   * Send a payload to the Chrome app.  Only "emit" messages are allowed.
-   * If currently connected to the App, immediately send. Otherwise, queue
-   * the message until connection completes.
-   * If skipQueue==true, payloads will not be enqueued disconnected.
-   */
-  public send = (payload :browser_connector.Payload,
-                 skipQueue :Boolean = false) => {
-    if (payload.cmd !== 'emit') {
-       throw new Error('send can only be used for emit');
+  
+    constructor(private options_ ?:chrome.runtime.ConnectInfo) {
+      this.status = { connected: false };
+      this.appChannel_ = null;
     }
-    if (skipQueue) {
-      return;
+  
+  
+    // --- Connectivity methods ---
+  
+    /**
+     * Connect the UI to the Freedom module.
+     *
+     * Returns a promise fulfilled upon connection.
+     */
+    public connect = () : Promise<void> => {
+      console.log('trying to connect to app');
+      if (this.status.connected) {
+        console.warn('Already connected.');
+        return Promise.resolve();
+      }
+  
+      return this.connect_().then(() => {
+        this.fulfillConnect_();
+        this.emit('core_connect');
+      });
     }
-    this.onceConnected.then(() => {
-      this.appChannel.emit('' + payload.type,
-          {data: payload.data, promiseId: payload.promiseId});
-    });
-  }
-
-  public flushQueue = () => {
-  }
-
-  public restart() {
-  }
-
-  private events :{[name :string] :Function} = {};
-
-  public on = (name :string, callback :Function) => {
-    this.events[name] = callback;
-  }
-
-  private emit = (name :string, ...args :Object[]) => {
-    if (name in this.events) {
-      this.events[name].apply(null, args);
+  
+    /**
+     * Promise internal implementation of the connection sequence.
+     * This relies on ui_context.uProxyAppChannel being created in the core
+     * context before connect() is called here in the UI context.
+     */
+    private connect_ = () : Promise<void> => {
+      return new Promise<void>((F,R) => {
+        // chrome.runtime.getBackgroundPage((bgPage) => {
+        //   (<any>bgPage).ui_context.uProxyAppChannel.then((channel:any) => {
+        //     this.appChannel_ = channel;
+        //     F();
+        //   });
+        // });
+        (<any>chrome.extension.getBackgroundPage()).ui_context.uProxyAppChannel.then((channel:any) =>{
+          this.appChannel_ = channel;
+          F();
+        });
+      });
     }
-  }
+  
+    // --- Communication ---
+    /**
+     * Attach handlers for updates emitted from the uProxy Core.
+     */
+    public onUpdate = (update :uproxy_core_api.Update,
+        handler :(eventData:any) => void) => {
+      this.onceConnected.then(() => {
+        var type = '' + update;
+        this.appChannel_.on(type, handler);
+      });
+    }
+  
+    /**
+     * Send a payload to the Chrome app.  Only "emit" messages are allowed.
+     * If currently connected to the App, immediately send. Otherwise, queue
+     * the message until connection completes.
+     * If skipQueue==true, payloads will not be enqueued disconnected.
+     */
+    public send = (payload :browser_connector.Payload,
+                   skipQueue :Boolean = false) => {
+      if (payload.cmd !== 'emit') {
+         throw new Error('send can only be used for emit');
+      }
+      if (skipQueue && !this.appChannel_) {
+        return;
+      }
+      this.onceConnected.then(() => {
+        this.appChannel_.emit('' + payload.type,
+            {data: payload.data, promiseId: payload.promiseId});
+      });
+    }
+  
+    public flushQueue = () => {
+    }
+  
+    public restart() {
+    }
+  
+    private events_ :{[name :string] :Function} = {};
+  
+    public on = (name :string, callback :Function) => {
+      this.events_[name] = callback;
+    }
+  
+    private emit = (name :string, ...args :Object[]) => {
+      if (name in this.events_) {
+        this.events_[name].apply(null, args);
+      }
+    }
 }  // class DesktopCoreConnector
