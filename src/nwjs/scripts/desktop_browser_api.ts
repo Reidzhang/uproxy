2afb677b0cd6bd7f7a983116c41d1e5c54c5eae7
Zhitao (Reid) Zhang <zzt124@cs.washington.edu>
2017-12-08 19:39:04 -0800

    desktopapi constructor

--- src/nwjs/scripts/desktop_browser_api.ts
+++ src/nwjs/scripts/desktop_browser_api.ts
@@ -2,6 +2,7 @@ import * as browser_api from "../../interfaces/browser_api";
 import BrowserAPI = browser_api.BrowserAPI;
 import * as net from '../../lib/net/net.types';
 import * as Constants from '../../generic_ui/scripts/constants';
+import * as MacProxy from './mac_set_proxy';
 import ProxyAccessMode = browser_api.ProxyAccessMode;
 import ProxyDisconnectInfo = browser_api.ProxyDisconnectInfo;
 
@@ -32,9 +33,12 @@ export default class DesktopBrowserApi implements BrowserAPI {
 
     // TODO: set the system VPN using the script generated before.
     private proxyAccessMode_ = ProxyAccessMode.NONE;
+    
     // When we tried to create UI.
     private popupCreationStartTime_ = Date.now();
+    
     // TODO: Maybe I should check on the the window
+    // popup is the main window
     private popupState_ = PopupState.NOT_LAUNCHED;
 
     private popup_url = "../../generic_ui/index.html";
@@ -45,22 +49,27 @@ export default class DesktopBrowserApi implements BrowserAPI {
     public handlePopupLaunch :() => void; 
 
     constructor() {
-        // TODO: add Notification
+        // add Notification
         chrome.notifications.onClicked.addListener((tag) => {
             this.emit_('notificationclicked', tag);
         });
-
+        
+        // clear the system proxy setting if there is any
+        MacProxy.stopProxy();
     }
     
     public setIcon = (iconFile :string) : void => {
+        // Void
     }
 
     public launchTabIfNotOpen = (relativeUrl :string) => {
+        // not sure what to handle for Nwjs, log the relativeurl so we know the input first
+        console.log('LaunchTabIfNotOpen called :' + relativeUrl);
     }
 
     public startUsingProxy = (endpoint: net.Endpoint, bypass: string[],
         opts: browser_api.ProxyConnectOptions) => {
-
+        // TODO: printout the endpoint.address.
     }
 
     public openTab = (url :string) => {
@@ -78,9 +87,10 @@ export default class DesktopBrowserApi implements BrowserAPI {
 
     public showNotification = (text :string, tag :string) => {
         var notification = new Notification('uProxy', {
-        body: text,
-        icon: 'icons/38_' + Constants.DEFAULT_ICON,
-        tag: tag});
+            body: text,
+            icon: 'icons/38_' + Constants.DEFAULT_ICON,
+            tag: tag
+        });
 
         notification.onclick = () => {
             this.emit_('notificationClicked', tag);
@@ -93,6 +103,7 @@ export default class DesktopBrowserApi implements BrowserAPI {
     public setBadgeNotification = (notification :string) => {
         // chrome.browserAction.setBadgeText({text: notification});
         // TODO : not sure how to handle this for the moment
+        this.showNotification(notification, 'badge');
     }
     
     private events_ :{[name :string] :Function} = {};
